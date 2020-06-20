b85ecac872219570f96be7cc332a6851146c39bd
yinuohan <31676123+yinuohan@users.noreply.github.com>
2020-06-20 20:27:53 +1000

    Update kepler_solve.m

--- model/kepler_solve.m
+++ model/kepler_solve.m
@@ -3,28 +3,33 @@ function theta = kepler_solve(t, P, ecc)
     tol = 1e-8; % Convergence tolerance
     
     M = 2*pi/P*t;
-    E = zeros(length(t));
-    tj = 0;
+    E = zeros(1,length(t));
+    %tj = 0;
     
     for i = 1:length(t)
         E0 = M(i);
+        %E1s = zeros(1,length(t))
+        
         % Newton's formula to solve for eccentric anomoly
         for j = 1:maxj
             E1 = E0 - (E0 - ecc*sin(E0)-M(i))/(1 - ecc*cos(E0));
+           %E1s(j) = E1;
             if abs(E1 - E0) < tol
-                E0 = E1;
+                break
             end
+            E0 = E1;
         end
-                
+        
         if j == maxj
-            %disp('Did not converge')
+            disp('Did not converge')
         end
-    
+        
         E(i) = E1;
-        tj = tj+j;
+        %tj = tj+j;
     end
     
-    % Compute 2-dimensional spiral angles & radii
+    %plot(E1s)
+    
+    % Convert eccentric anomaly to true anomaly
     theta = 2*atan(sqrt((1+ecc)/(1-ecc))*tan(E/2));
-    % I changed the sign to negative 
 end
